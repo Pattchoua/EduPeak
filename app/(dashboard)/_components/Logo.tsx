6807e590f6b3f8ea8663e811fd90e5b6dbebda10
Patrick Tchoua <tchouapatrick@gmail.com>
2023-10-24 15:35:28 +0200

    navbar implemented

---
+++ app/(dashboard)/_components/Logo.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+const Logo = () => {
+  return <Image 
+  src="/assets/images/logo-site.png"
+  alt="logo"
+  height={100}
+  width={100}
+  />;
+};
+
+export default Logo;
